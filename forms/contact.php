2f1195b9ddc15bba9d97bef4adbc53c704017c98
Luis Daniel Landa Tesillos <d.landa@ralken.com.mx>
2023-10-13 23:53:42 -0600

    contacto

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,25 @@
-<?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+<?php 
+// Esto es para activar la visualización de errores en el servidor, por si los hubiese
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+$name = $_POST['name'];
+$from = $_POST['email'];
+$telefono = $_POST['telefono'];
+$message = $_POST['message'];
+$subject = "Prueba";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+// El from DEBE corresponder a una cuenta de correo real creada en el servidor
+$headers = "From: contacto@citricoscs.com\r\n"; 
+$headers .= "Reply-To: $from\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=utf-8\r\n"; 
+ 
+if(mail($from, $subject, $message,$headers)){
+ echo "mail enviado";
+ }else{
+ $errorMessage = error_get_last()['msg'];
+ echo $errorMessage;
+}
 ?>
