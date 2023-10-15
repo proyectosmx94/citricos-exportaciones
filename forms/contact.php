b2136a5a7270cd5d8bca63c38bcb199b30914728
Luis Daniel Landa Tesillos <d.landa@ralken.com.mx>
2023-10-15 16:25:01 -0600

    Acentos

--- forms/contact.php
+++ forms/contact.php
@@ -8,17 +8,15 @@ $mensaje = $_POST['message'];
 
 // Datos del email
 $para = "contacto@citricoscs.com";
-$titulo = 'Solicitud de Informacioón';
+$titulo = 'Solicitud de Informacion';
 $header = 'From: ' . $email;
-$msjCorreo = "Nombre: $nombre\n Teléfono: $telefono\n E-Mail: $email\n Mensaje:\n $mensaje";
+$msjCorreo = "Nombre: $nombre\n Telefono: $telefono\n Correo: $email\n Mensaje:\n $mensaje";
 
 
 
     if (mail($para, $titulo, $msjCorreo, $header)) {
 
-    echo "<script language='javascript'>
-    alert('Mensaje enviado, muchas gracias por contactar con nosotros.');
-    </script>";
+    echo 'OK';
     } else {
         echo 'Falló el envio';
     }
